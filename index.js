a1896a313382e7445b43cccb30891a3aca9992f7
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-01 22:05:54 -0500

    practicing JS

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//initialize the count as 0
+//listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
+
+function increment () {
+    console.log("The button was clicked")
+}
 
 
