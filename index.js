c85b8ef2852fafb737e218fb73c1570e54ab4565
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-03 08:15:00 -0500

    js practice, writing functions

--- index.js
+++ index.js
@@ -6,4 +6,44 @@
 function increment() {
     console.log("The button was clicked")
 }
+function countdown() {
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+}
+
+countdown()
+countdown()
+
+//Create a function that logs out the sum of all the lap times
+
+//let lap1 = 34
+//let lap2 = 33
+//let lap3 = 36
+
+//function sum(){
+//    console.log(lap1 + lap2 + lap3)
+//}
+//sum();
+
+//I can do this another way as well
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function sum() {
+    let totalLapTime = lap1 + lap2 + lap3
+    console.log(totalLapTime)
+}
+
+sum()
+//write a function that increments. We want to 
+//write a function that logs the amount of
+//laps the player runs.
+//Step 1: Create a function that increments the lapsCompleted
+//variable with 1.
+//Run it three times
 
