a5757ae82b9561c6e5625e1b961995c358ee2315
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-04 15:24:44 -0500

    working on lesson 12

--- index.js
+++ index.js
@@ -36,12 +36,23 @@ console.log(lapsCompleted)
 
 
 
+let countEl = document.getElementById("count-el")
 
 let count = 0
 
 function increment(){
 count = count + 1    
-}
+countEl.innerText = count
 console.log(count)
+}
 
-let countEl = document.getElementById("increment-btn")
+// Create a function that logs out the sum of all the lap times
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function logLapTime(){
+    let lapTime = lap1 + lap2 + lap3
+    console.log(lapTime)
+}
+logLapTime()
