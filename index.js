7cf934a33ac4d6157b62644ba261b07a0ea70f8e
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-04 12:47:00 -0500

    added new js code

--- index.js
+++ index.js
@@ -6,3 +6,42 @@
 function increment() {
     console.log("The button was clicked")
 }
+
+function countdown(){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)    
+}
+countdown()
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function totalLaps() {
+   lapsCompleted = lapsCompleted + 1
+}
+
+totalLaps()
+totalLaps()
+totalLaps()
+
+console.log(lapsCompleted)
+
+
+
+
+
+
+
+let count = 0
+
+function increment(){
+count = count + 1    
+}
+console.log(count)
+
+let countEl = document.getElementById("increment-btn")
