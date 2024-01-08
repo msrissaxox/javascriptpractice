5d6bf1d2b138d6b0c493f47bfea99a5cec366a15
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-08 12:43:33 -0500

    added the countEl variable to the JS

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 //increment the count variable when the button is clicked
 //change the count-el in the HTML to reflect the new count
 
+let countEl = document.getElementById("count-el")
+
 let count = 0
 function increment() {
     count = count + 1
