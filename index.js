614ae601ef331a6803f9da51e35f01c416ba9b78
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-08 12:41:19 -0500

    added javascript and tested it in Chrome. The log count works. Next, I will change the count-el to reflect the new number in the html doc

--- index.js
+++ index.js
@@ -6,29 +6,11 @@
 //change the count-el in the HTML to reflect the new count
 
 let count = 0
-
-function increment(){
-    console.log("button was clicked")
-}
-
-
-let count = 0
-function increment(){
+function increment() {
     count = count + 1
     console.log(count)
 }
-let lapsCompleted = 0
-
-// Create a function that increments the lapsCompleted variable with one
-// Run it three times
-
-function sumOfLaps(){
-    lapsCompleted = lapsCompleted + 1
-    console.log(lapsCompleted)
-}
-sumOfLaps()
-sumOfLaps()
-sumOfLaps()
 
 
-console.log(lapsCompleted)
+
+
