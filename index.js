3cedd7d77d1b46f2808e4c678a1c763e86d53ec4
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-12 21:10:11 -0500

    added another project below to attempt javascript buttons

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
 //increment the count variable when the button is clicked
 //change the count-el in the HTML to reflect the new count
 
+let count = 0
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
