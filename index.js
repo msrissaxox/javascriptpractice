03f54121db75f8e8775ed142ea502db37746b990
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-24 09:31:04 -0500

    adding new activity

--- index.js
+++ index.js
@@ -60,3 +60,26 @@ console.log(3 < 0)    // false
 console.log(3 >= 3)   // true
 console.log(11 <= 11) // true
 console.log(3 <= 2)   // false
+
+
+//New assignment
+
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
+// 1. Declare a variable called message and assign its value to an empty string
+
+// 2. Reassign the message variable to the string we're logging out
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("You're out of the game! 😭")
+    isAlive = false
+}
+
+// 3. Log it out!
