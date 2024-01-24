2740df218dba2a3c8d4db7b1f125253f3428f91b
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-01-24 09:23:53 -0500

    added boolean practice

--- index.js
+++ index.js
@@ -52,3 +52,11 @@ let message = document.getElementById("welcome-message")
 
 message.textContent = welcomeMessage + ", " + yourName
 message.textContent += "👋"
+
+console.log(4 === 3)  // false
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false
+console.log(3 < 0)    // false
+console.log(3 >= 3)   // true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   // false
